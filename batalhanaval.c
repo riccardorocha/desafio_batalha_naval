464c9a7f787b52dc4659f9502fe5d902e81dfd67
Ricardo <riccardorocha@gmail.com>
2025-09-24 19:46:48 +0000

    Atualização do Desafio Batalha Naval - Mestre

--- batalhanaval.c
+++ batalhanaval.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
  
+#include <stdio.h>
+ 
 int main() {
     int tabuleiro[10][10];
     char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
@@ -7,48 +9,46 @@ int main() {
 
     //Criando o tabuleiro 10 x 10
     for(i = 0; i < 10; i++) 
-    {        
-        //printf("%d  ", i+1);
-         
+    {    
         for(j = 0; j < 10; j++)
         {   
             tabuleiro[i][j] = 0; 
         }
         
     }
+   
 
-    //Posição do navio horizontal na linha 2, colunas 5, 6 e 7
+    //Posição do navio horizontal na linha 0, colunas 0, 1 e 2
     for (int i = 0; i < 10; i++)
     {
-        for (int j = 1; j < 10; j++)
-        
-        if (j > 4 && j < 8)
+        for (int j = 0; j < 10; j++)
         {
-            if (tabuleiro[2][j] == 0)
+        if (j > -1 && j < 3)
+        {
+            if (tabuleiro[0][j] == 0)
             {
-                tabuleiro[2][j] = 3;
+                tabuleiro[0][j] = 3;
             }
+        }
         }   
     }
 
-    //Posição do navio vertical na coluna 2, linhas 2, 3 e 4
+    //Posição do navio vertical na coluna 0, linhas 2, 3 e 4
     for (int i = 0; i < 10; i++)
-    {
-        for (int j = 1; j < 10; j++)
-        
+    {            
         if (i > 1 && i < 5)
         {
-            if (tabuleiro[i][2] == 0)
+            if (tabuleiro[i][0] == 0)
             {
-                tabuleiro[i][2] = 3;
+                tabuleiro[i][0] = 3;
             }
         }   
     }
 
     //Posição do navio na coluna principal nas linhas 5, 6 e 7
-    for (int i=0; i < 8; i++)
+    for (int i = 0; i < 10; i++)
     {
-        if (i > 4)
+        if (i > 6)
         {
             if (tabuleiro[i][i] == 0)
             {
@@ -58,9 +58,9 @@ int main() {
     }
     
     //Posição do navio na coluna secundária nas linhas 6, 7 e 8
-    for (int i=0; i < 9; i++)
+    for (int i = 0; i < 10; i++)
     {
-        if (i > 5)
+        if (i > 6)
         {
             if (tabuleiro[i][9-i] == 0)
             {
@@ -69,23 +69,88 @@ int main() {
         }   
     }
 
+    //Posição do cone 5 x 5 - Representado pelo n. 5
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+        if (i == 4 && j == 2)
+        {
+            tabuleiro[i][j] = 5;
+            tabuleiro[i+1][j] = 5;
+            tabuleiro[i+1][j-1] = 5;
+            tabuleiro[i+1][j+1] = 5;
+            tabuleiro[i+2][j] = 5;
+            tabuleiro[i+2][j-1] = 5;
+            tabuleiro[i+2][j-2] = 5;
+            tabuleiro[i+2][j+1] = 5;
+            tabuleiro[i+2][j+2] = 5;
+        }
+        }   
+    }
 
-    //IMPRINDO OS RESULTADOS
+    //Posição da cruz 5 x 5 - Representado pelo n. 6
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+        if (i == 0 && j == 4)
+        {
+            tabuleiro[i][j] = 6;
+            tabuleiro[i+1][j] = 6;
+            tabuleiro[i+2][j] = 6;
+            tabuleiro[i+2][j-1] = 6;
+            tabuleiro[i+2][j-2] = 6;
+            tabuleiro[i+2][j+1] = 6;
+            tabuleiro[i+2][j+2] = 6;
+            tabuleiro[i+3][j] = 6;
+            tabuleiro[i+4][j] = 6;
+        }
+        }   
+    }
+
+    //Posição do octaedro 5 x 5 - Representado pelo n. 7
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+        if (i == 2 && j == 7)
+        {
+            tabuleiro[i][j] = 7;
+            tabuleiro[i+1][j] = 7;
+            tabuleiro[i+1][j-1] = 7;
+            tabuleiro[i+1][j+1] = 7;
+            tabuleiro[i+2][j] = 7;
+            tabuleiro[i+2][j-1] = 7;
+            tabuleiro[i+2][j-2] = 7;
+            tabuleiro[i+2][j+1] = 7;
+            tabuleiro[i+2][j+2] = 7;
+            tabuleiro[i+3][j] = 7;
+            tabuleiro[i+3][j-1] = 7;
+            tabuleiro[i+3][j+1] = 7;
+            tabuleiro[i+4][j] = 7;
+        }
+        }   
+    }
+
+    
+//IMPRINDO OS RESULTADOS
 
     printf("\n");
     printf("  ******** JOGO BATALHA NAVAL  ******** \n\n");
 
     //Imprimindo a linha referência das colunas/letras
+    printf(" ");
     for(j = 0; j < 10; j++) 
     {
         printf("   %c", linha[j]);
     }
-    printf("\n");
+    printf("\n\n");
 
     //Exibindo as posições 0 de água e as posições 3 dos navios
     for(i=0; i<10; i++) 
     {        
-        printf("%d  ", i+1); //Imprimindo a  referência das linhas
+        printf("%d   ", i); //Imprimindo a referência das linhas
          
         for(j = 0; j < 10; j++)
         {
